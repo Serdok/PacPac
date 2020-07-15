dc9fa9ef15ed6a340a95eba38363f22fb827b663
Anass 'Serdok' Lahnin <l.anass.pro@gmail.com>
2020-07-16 00:38:44 +0200

    :alembic: Testing the game class

--- Graphics/Text/main.cpp
+++ Graphics/Text/main.cpp
@@ -3,24 +3,36 @@
 // The aim of this executable is to have a "playground" to test basic functionalities before going 2D (or more ? :p)
 //
 
-#include <PacPac/Level.h>
+#include <PacPac/Game.h>
+
 #include <iostream>
 #include <stdexcept>
 
-#include <vector>
 
 int main( [[maybe_unused]] int argc, [[maybe_unused]] char* argv[] ) {
     try {
-        std::filesystem::path assets_path = std::filesystem::path( argv[0] ).parent_path();
-        assets_path /= "..";
-        assets_path /= "..";
-        assets_path /= "..";
-        assets_path /= "assets";
-        std::cout << std::filesystem::canonical( assets_path ) << '\n';
+        Game game( argc, argv );
+        game.loadLevel( "levels/level0.json" );
 
-        entt::registry r;
+        auto start = std::chrono::steady_clock::now();
+        auto total_time = 0us;
+        constexpr std::chrono::microseconds end = 15s;
+        while (not game.isDone()) {
+            // Update clock
+            auto now = std::chrono::steady_clock::now();
+            auto dt = std::chrono::duration_cast< std::chrono::microseconds >(now - start);
+            total_time += dt;
+            start = now;
 
-        Level l( assets_path / "levels/level0.json", r );
+            if (total_time.count() >= end.count()) {
+                // End the game
+                game.publish< GameEnd >();
+            }
+
+            // Update game
+            game.update( dt );
+
+        }
 
         return 0;
     } catch (std::ios_base::failure& e) {
